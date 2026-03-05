bc124c885d63f68116fffb24bcd777f2404e4aa2
alphaleadership <alphaleadershipyt@protonmail.com>
2026-03-05 09:59:10 +0000

    force domain

--- domain.js
+++ domain.js
@@ -1,15 +1,19 @@
+
 const lt=require("localtunnel")
 const http=require("https")
 lt({port:3000,subdomain:"partagesiochaptal"}).then(tunnel=>{
     console.log("tunnel url:",tunnel.url)
+	if(tunnel.url!=="https://partagesiochaptal.loca.lt"){process.exit(0)}
+    console.log(tunnel.tunnelCluster)
     require("fs").writeFileSync("tunnel.txt",tunnel.url)
+    process.on("exit",(t)=>{tunnel.close()})
     // Autorise tous les certificats (attention : sécurité réduite)
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-    http.get("https://loca.lt/185.233.130.41", {
+    //process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+/*    http.get("https://loca.lt/185.233.130.41", {
         headers: {
             "Host": "partages.io",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*\/*;q=0.8",
             "Accept-Language": "en-US,en;q=0.5",
             "Accept-Encoding": "gzip, deflate, br",
             "Connection": "keep-alive",
@@ -27,7 +31,7 @@ lt({port:3000,subdomain:"partagesiochaptal"}).then(tunnel=>{
         })
     }).on("error", e => {
         console.error(e)
-    })
+    })*/
 }).catch(e=>{
     console.error(e)
 })
